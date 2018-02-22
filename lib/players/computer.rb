4966508c1a3c8bff2a53b2c90b96b4c8f621f692
Sean Oughton <seanoughton@gmail.com>
2018-02-22 17:04:31 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -36,10 +36,10 @@ module Players
       array_of_empty_cells = []
       board.cells.each_with_index do |element,index|
         if element == " "
-          array_of_empty_cells << index+1
+          array_of_empty_cells << (index+1).to_s
         end
       end
-      array_of_empty_cells[0].to_s
+      array_of_empty_cells[0]
 
 
 
