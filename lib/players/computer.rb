aa35971a772406f9efebc485b5b78828c2df2545
Sean Oughton <seanoughton@gmail.com>
2018-02-22 19:11:55 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -42,7 +42,6 @@ module Players
       def defense#provides defenesive/blocking moves
         #need a blocking method that takes into account where each player has played
         opponent_cells #where the opponent is on the board
-        @the_board.cells
         #if the O's are lined up in such a way that the next move will be a win
         #the move to block, this takes precedence over the offensive strategy
       end
