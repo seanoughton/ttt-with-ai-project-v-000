8e34de806102adfc31de4052a628266280f3bc5a
Sean Oughton <seanoughton@gmail.com>
2018-02-22 18:55:13 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -25,6 +25,7 @@ module Players
         elsif self.token == "O"
           opponent = "X"
         end
+        puts opponent
 
         #assuming computer is "X" : check to see where the O's are on the board
         #if the O's are lined up in such a way that the next move will be a win
@@ -35,7 +36,7 @@ module Players
 
       def take_corners_strategy #take these positions: 1,3,9 : 3,7,9: 1,7,9: 1,3,7
 
-        #defense
+        defense
         empty_cells
         #find the first of these numbers that is not in empty_cells and return that element
         next_move = empty_cells.select do |element|
