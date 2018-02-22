b2893e79387f1b78c6b57d1c242b2de16519aa96
Sean Oughton <seanoughton@gmail.com>
2018-02-22 22:28:09 +0000

    automatically backed up by learn

--- lib/player.rb
+++ lib/player.rb
@@ -1,15 +1,8 @@
 class Player
-
   attr_reader :token
 
   def initialize(token) #this might need to be an optional argument
     #this might not be correct
     @token = token
   end
-
-#not sure that this method is necessary here
-  #def move
-    #Every player subclass will implement a #move method that represents how that type of player makes a move in Tic-tac-toe.
-  #end
-
 end
