cb412fd9adf62407e82b686bdd47d99d48c6ccd0
Sean Oughton <seanoughton@gmail.com>
2018-02-22 17:01:44 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -16,6 +16,7 @@ module Players
       #take the corners strategy
       #take these positions: 1,3,9 : 3,7,9: 1,7,9: 1,3,7
       def take_corners_strategy
+        #get the empty positions: call method empty_cells
       end
 
 
