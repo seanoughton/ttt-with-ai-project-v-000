aebfcf34ecd6d6ad7eb1bb5f8f9589ec0058613c
Sean Oughton <seanoughton@gmail.com>
2018-02-22 18:56:54 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -26,6 +26,7 @@ module Players
           opponent = "X"
         end
         #check to see where the opponent's moves are on the board
+        @the_board
         #if the O's are lined up in such a way that the next move will be a win
         #the move to block, this takes precedence over the offensive strategy
       end
