6dc7b1e58432e336417fb94665f9b3568d849d47
Sean Oughton <seanoughton@gmail.com>
2018-02-22 22:11:07 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,10 +1,6 @@
-#Define a class Human that inherits from Player. This class should be namespaced inside the module Players
-
-#when you call your Human class, you'll have to call it inside its name space with Players::Human
-
 module Players
   class Human < Player
-    
+
     def move(board)
       #takes in a board argument and allows a human player to enter a move via the CLI.
       #The method should return the value the user enters.
