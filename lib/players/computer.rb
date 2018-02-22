53e91779b4238d050505410b83f912fc4df866b7
Sean Oughton <seanoughton@gmail.com>
2018-02-22 18:00:53 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,7 +7,6 @@ module Players
   class Computer < Player
     def move(board)
       #accepts a board and returns the move the computer wants to make in the form of a 1-9 string.
-
       @the_board = board
       def empty_cells
         array_of_empty_cells = []
